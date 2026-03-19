9e32e33e9b7374d5b1151c0aeda3b4e2743a2230
anky894 <anky894@proton.me>
2026-03-20 01:31:39 +0530

    avalon: Update from OOS 11.F.14
    Change-Id: Iefb239c0361ba926cac14713fd512861adcda46a

--- lineage_avalon.mk
+++ lineage_avalon.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := CPH2661
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 16 BP2A.250605.015 1767520364211 release-keys" \
-    BuildFingerprint=OnePlus/CPH2661IN/OP5E93L1:16/UKQ1.231108.001/U.R4T2.2bc704f-9dd71a-9dd71b:user/release-keys \
+    BuildDesc="qssi_64-user 16 BP2A.250605.015 1773714753851 release-keys" \
+    BuildFingerprint=OnePlus/CPH2661IN/OP5E93L1:16/UKQ1.231108.001/U.R4T2.106fa18_129a409_127900d:user/release-keys \
     DeviceName=OP5E93L1 \
     DeviceProduct=CPH2661 \
     SystemDevice=OP5E93L1 \
