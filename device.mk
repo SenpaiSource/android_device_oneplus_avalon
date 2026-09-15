f48d6c1975ea179264685697796cae4e432ce058
adithya2306 <hiroshi@tanuki.page>
2026-09-15 19:21:40 +0530

    overlay: Silence some spammy logging

--- device.mk
+++ device.mk
@@ -72,6 +72,18 @@ $(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.lunaris.maintainer=SATHIYA_SENPAI
 
+# Silence spammy vendor logs
+SPAMMY_LOG_TAGS := \
+    Diag_Lib \
+    AGM \
+    AHAL \
+    CamX
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=S)
+endif
+
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/oneplus/sm8650-common/common.mk)
 
