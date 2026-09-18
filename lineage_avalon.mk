983b533521fc572a7b1ead055c2afd78bc587e99
EternalMikaelson <klaus_mikaelson@rediffmail.com>
2026-09-18 17:42:47 +0000

    avalon: Update from OOS 11.F.17
    Change-Id: I5571d7317567c12b68c7484c855c69b66e182863
    Signed-off-by: EternalMikaelson <klaus_mikaelson@rediffmail.com>

--- lineage_avalon.mk
+++ lineage_avalon.mk
@@ -37,8 +37,8 @@ BYPASS_CHARGE_SUPPORTED := true
 USE_REALITY_ENGINE := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 16 BP2A.250605.015 1782873585026 release-keys" \
-    BuildFingerprint=OnePlus/CPH2661IN/OP5E93L1:16/UKQ1.231108.001/U.R4T2.3a24ca8-1593c70-163c453:user/release-keys \
+    BuildDesc="qssi_64-user 16 BP2A.250605.015 1788854475117 release-keys" \
+    BuildFingerprint=OnePlus/CPH2661IN/OP5E93L1:16/UKQ1.231108.001/U.R4T2.202609082125:user/release-keys \
     DeviceName=OP5E93L1 \
     DeviceProduct=CPH2661 \
     SystemDevice=OP5E93L1 \
